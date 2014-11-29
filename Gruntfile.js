cea0a457ed71fcfa27e5eb98d44d15690f1985ed
Anthony Skelton <anthony@redcartstudios.com>
2014-11-29 06:21:10 -0800

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	
+	concat: {
+		dist: {
+			src: [
+				'library/js/libs/*.js',
+				'library/js/scripts.js'
+			],
+			dest: 'library/js/production.js',
+		}
+	},
+	uglify: {
+		build: {
+			src: 'library/js/production.js',
+			dest: 'library/js/production.min.js'
+		}
+	},
+	compass: {
+		dist: {
+			options: {
+				sassDir: 'library/scss',
+				cssDir: 'library/css'
+			}
+		}
+	},
+	autoprefixer: {
+		dist: {
+			files: {
+				'library/css/production-style.css': 'library/css/style.css'
+			}
+		}
+	},
+	svgstore: {
+		options: {
+			prefix : 'shape-',
+			cleanup: false,
+			svg: {
+				style: "display: none;"
+			}
+		},
+		default : {
+			files: {
+				'library/svg-defs.svg' : ['library/svg/*.svg'],
+			}
+		}
+	},
+	watch: {
+		options: {
+			livereload:false,
+		},
+		scripts: {
+			files: ['library/js/*.js'],
+			tasks: ['concat', 'uglify', 'notify:scripts'],
+			options: {
+				spawn: false,
+			},
+		},
+		css: {
+			files: '**/*.scss',
+			tasks: ['compass', 'autoprefixer', 'notify:css']
+		},
+	},
+	notify: {
+		css: {
+			options: {
+				title: 'Task Complete',
+				message: 'Compass/Autoprefixer finished compiling!'
+			}
+		},
+		scripts: {
+			options: {
+				title: 'Task Complete',
+				message: 'Scripts finished compiling!'
+			}
+		}
+	}
+	});
+
+	require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-notify');
+
+	grunt.registerTask('default', ['watch']);
+
+};
---
+++ page-homepage.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ Template Name: Homepage
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="hero">
+		<div class="parallax">
+			<div class="parallax__layer parallax__layer--back-hero"></div>
+			<div class="parallax__layer parallax__layer--base-hero">
+				<p class="hero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus corrupti dicta odit perspiciatis, at harum sit perferendis aspernatur numquam error iure odio pariatur, eius laboriosam cupiditate eligendi alias id dolor.</p>
+			</div>
+		</div>
+	</div>
+
+		<div id="about" class="cf">
+			<div class="m-all t-1of2 d-1of2">
+				<img src="<?php echo get_template_directory_uri(); ?>/library/images/katrinaportrait.jpg" alt="">
+			</div>
+			<div class="m-all t-1of2 d-1of2 last-col">
+				<p>
+					<?php the_field('about_section'); ?>
+				</p>
+			</div>
+		</div>
+
+	<div class="parallax">
+		<div class="parallax__layer parallax__layer--back"></div>
+		<div class="parallax__layer parallax__layer--base"></div>
+	</div>
+
+		<div id="music" class="cf">
+			<h2>Music</h2>
+			<div class="wrap">
+				<?php the_field('music_section'); ?>
+			</div>
+		</div>
+
+	<div class="parallax">
+		<div class="parallax__layer parallax__layer--back"></div>
+		<div class="parallax__layer parallax__layer--base"></div>
+	</div>
+
+		<div id="shows" class="cf">
+			<h2>Upcomming Shows</h2>
+			<div class="wrap">
+				<?php the_field('shows_section'); ?>
+			</div>
+			<h3>Recent Shows</h3>
+			<div class="wrap">
+				<?php the_field('shows_archive'); ?>
+			</div>
+		</div>
+
+	<div class="parallax">
+		<div class="parallax__layer parallax__layer--back"></div>
+		<div class="parallax__layer parallax__layer--base"></div>
+	</div>
+
+		<div id="videos">
+			
+		</div>
+
+	<div class="parallax">
+		<div class="parallax__layer parallax__layer--back"></div>
+		<div class="parallax__layer parallax__layer--base"></div>
+	</div>
+
+		<div id="contact">
+			<h2>Contact Me</h2>
+			<div class="wrap">
+				<?php echo do_shortcode( '[contact-form-7 id="887" title="Contact form 1"]' ); ?>
+			</div>
+		</div>
+
+<?php get_footer(); ?>
