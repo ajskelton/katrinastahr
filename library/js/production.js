4a8ff0b5a65314d62f5c295344695def6b0afd62
Anthony Skelton <anthony@redcartstudios.com>
2014-11-30 07:58:02 -0800

    Added scripts for link scrolling

--- library/js/production.js
+++ library/js/production.js
@@ -124,5 +124,18 @@ jQuery(document).ready(function($) {
     }
   });
 
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
 
 }); /* end of as page load scripts */
