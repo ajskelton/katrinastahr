747c2189b7eb6e9faf1120031aba004d82babd15
Anthony Skelton <anthony@redcartstudios.com>
2015-02-14 05:39:20 -0800

    Added functions to get average color of jpg image, and calculate white or black text based on the contrast of that color.

--- functions.php
+++ functions.php
@@ -242,4 +242,63 @@ function bones_fonts() {
 }
 add_action('wp_enqueue_scripts', 'bones_fonts');
 
+
+/************* CUSTOM FUNCTIONS *********************/
+
+function colorPalette($imageFile, $numColors, $granularity = 5) 
+{ 
+   $granularity = max(1, abs((int)$granularity)); 
+   $colors = array(); 
+   $size = @getimagesize($imageFile); 
+   if($size === false) 
+   { 
+      user_error("Unable to get image size data"); 
+      return false; 
+   } 
+   $img = @imagecreatefromjpeg($imageFile);
+   // Andres mentioned in the comments the above line only loads jpegs, 
+   // and suggests that to load any file type you can use this:
+   // $img = @imagecreatefromstring(file_get_contents($imageFile)); 
+
+   if(!$img) 
+   { 
+      user_error("Unable to open image file"); 
+      return false; 
+   } 
+   for($x = 0; $x < $size[0]; $x += $granularity) 
+   { 
+      for($y = 0; $y < $size[1]; $y += $granularity) 
+      { 
+         $thisColor = imagecolorat($img, $x, $y); 
+         $rgb = imagecolorsforindex($img, $thisColor); 
+         $red = round(round(($rgb['red'] / 0x33)) * 0x33); 
+         $green = round(round(($rgb['green'] / 0x33)) * 0x33); 
+         $blue = round(round(($rgb['blue'] / 0x33)) * 0x33); 
+         $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue); 
+         if(array_key_exists($thisRGB, $colors)) 
+         { 
+            $colors[$thisRGB]++; 
+         } 
+         else 
+         { 
+            $colors[$thisRGB] = 1; 
+         } 
+      } 
+   } 
+   arsort($colors); 
+   return array_slice(array_keys($colors), 0, $numColors); 
+} 
+
+function getContrast50($hexcolor){
+    return (hexdec($hexcolor) > 0xffffff/2) ? 'black':'white';
+}
+
+
+
+
+
+
+
+
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
