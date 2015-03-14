eeea3b707401036bd50234410bfca1dc4b995bb4
Anthony Skelton <anthony@redcartstudios.com>
2015-03-14 03:38:32 -0700

    Removed reverbnation history of shows widget

--- page-homepage.php
+++ page-homepage.php
@@ -86,7 +86,6 @@
 			<h2>Upcoming Shows</h2>
 			<div class="wrap">
 				<?php the_field('shows_section'); ?>
-				<div class="widget_iframe" style="display:inline-block;width:100%;height:550px;margin:0;padding:0;border:0;"><iframe class="widget_iframe" src="http://www.reverbnation.com/widget_code/html_widget/artist_4211802?widget_id=52&pwc[design]=default&pwc[background_color]=%23333333&pwc[layout]=detailed&pwc[show_map]=0%2C1&pwc[size]=fit" width="100%" height="100%" frameborder="0" scrolling="no"></iframe><div class="footer_branding" style="margin-top:-5px;font-size:10px;font-family:Arial;"><center><a href="http://www.reverbnation.com/band-promotion/how-to-sell-music-on-itunes?utm_campaign=a_features_distribution&utm_medium=widget&utm_source=HTML5_Show_Schedule&utm_content=widgetfooter_Sell music on Amazon at ReverbNation.com" target="_blank" style="text-decoration:none;color:#444;">Sell music on Amazon at ReverbNation.com</a></center></div></div>
 			</div>
 			<!-- <?php echo '<pre>'; print_r( _get_cron_array() ); echo '</pre>'; ?> -->
 			<h3>Past Shows</h3>
