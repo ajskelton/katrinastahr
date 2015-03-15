8953cfb32bc4319c9e730bc8525634219a3c0fe3
Anthony Skelton <anthony@redcartstudios.com>
2015-03-15 05:44:11 -0700

    Added back in Bones default helper script for throttling resize events

--- library/js/production.js
+++ library/js/production.js
@@ -37,17 +37,17 @@
  * Wrap your actions in this function to throttle the frequency of firing them off, for better performance, esp. on mobile.
  * ( source: http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed )
 */
-// var waitForFinalEvent = (function () {
-// 	var timers = {};
-// 	return function (callback, ms, uniqueId) {
-// 		if (!uniqueId) { uniqueId = "Don't call this twice without a uniqueId"; }
-// 		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
-// 		timers[uniqueId] = setTimeout(callback, ms);
-// 	};
-// })();
+var waitForFinalEvent = (function () {
+	var timers = {};
+	return function (callback, ms, uniqueId) {
+		if (!uniqueId) { uniqueId = "Don't call this twice without a uniqueId"; }
+		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
+		timers[uniqueId] = setTimeout(callback, ms);
+	};
+})();
 
 // how long to wait before deciding the resize has stopped, in ms. Around 50-100 should work ok.
-// var timeToWaitForLast = 100;
+var timeToWaitForLast = 100;
 
 
 /*
