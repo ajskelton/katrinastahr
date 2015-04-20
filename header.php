be655582c7dc29347470262f91c4adcda5b64c5b
Anthony Skelton <anthony@redcartstudios.com>
2015-04-20 12:37:58 -0700

    More testing for skrollr, added skrollr class to html

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js skrollr skrollr-desktop"><!--<![endif]-->
+<!-- <html class="skrollr skrollr-desktop"> -->
 	<head>
 		<meta charset="utf-8">
 
