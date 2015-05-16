27a2751b17e1024c8c7077ef555394464615134b
Anthony Skelton <anthony@redcartstudios.com>
2015-05-16 05:39:42 -0700

    Added header for tweets

--- page-homepage.php
+++ page-homepage.php
@@ -30,6 +30,7 @@
 			</header>
 			<div class="wrap">
 				<?php the_field('about_section'); ?>
+				<h3>Recent Tweets</h3>
 				<?php db_twitter_feed() ?>
 				<div class="quick-links">
 					<p>Follow Katrina on</p>
